d919d2ba89d380e31758cbe606cb547b878d5aeb
Mohammad Rahman <me@shadikur.com>
2023-05-05 14:41:11 +0200

    Environment variables updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,18 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: import.meta.env.REACT_APP_FB_APIKEY,
-  // authDomain: import.meta.env.REACT_APP_FB_AUTHDOMAIN,
-  // projectId: import.meta.env.REACT_APP_FB_PROJECTID,
-  // storageBucket: import.meta.env.REACT_APP_FB_STORAGEBUCKET,
-  // messagingSenderId: import.meta.env.REACT_APP_FB_MESSAGINGSENDERID,
-  // appId: import.meta.env.REACT_APP_FB_appId,
-  apiKey: "AIzaSyDLvCZjpCQOHak6mfPiO4Mu7wUuMkynz0M",
-  authDomain: "thai-chef-e1bc4.firebaseapp.com",
-  projectId: "thai-chef-e1bc4",
-  storageBucket: "thai-chef-e1bc4.appspot.com",
-  messagingSenderId: "518375992516",
-  appId: "1:518375992516:web:cea005c0a3f0a568af2d91",
+  apiKey: import.meta.env.VITE_FB_APIKEY,
+  authDomain: import.meta.env.VITE_FB_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECTID,
+  storageBucket: import.meta.env.VITE_FB_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FB_APPID,
 };
 
 // Initialize Firebase
